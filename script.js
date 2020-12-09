8c3327dd85ac55335779033b131df7661339a3de
Gaya-thri <74805362+Gaya-thri@users.noreply.github.com>
2020-12-09 18:46:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,43 @@ request.send();
 // the function is triggered when onload is ready with response 
 request.onload = function() {
     var data = JSON.parse(this.response);//JSON.parse to parse into perfect json object 
-    console.log(data);
+    
     var asiacountry = data.filter(function (elements,index){
         return elements.region==='Asia';
     });
     console.log(asiacountry);
+    
+    
     var population2 = data.filter(function (elements){
         return elements.population < 200000
    });
    console.log(population2);
+    
+    
     data.forEach(function(element) {
         console.log(element.name  +"      "+ element.capital +"        "+ element.flag);
    });
-   var totalpop = data.reduce(function(accvalue,current) {
+  
+    
+    var totalpop = data.reduce(function(accvalue,current) {
        //console.log(accvalue);
        return accvalue + current.population;
    },0)
    console.log(totalpop);
-   var uscountry = data.filter(function(elements) {
-                return elements.currencies[0].code==='USD';
-   })
-   console.log(uscountry);
+   
+    
+    var usd_country = data.filter((element) => {
+    let flag = false
+    element.currencies.forEach(elements => {
+        if(elements.code ==='USD') 
+            flag = true;
+    });
+    if(flag==true) 
+        return true;
+    else 
+        return false;
+})
+console.log(usd_country);
 
 }
     
